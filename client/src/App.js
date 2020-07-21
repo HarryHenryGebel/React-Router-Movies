ab50020dad89feda42e2fb20601668afbf3778d8
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-21 05:57:35 -0400

    Active movies/:id route

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,25 @@ import React, { useState, useEffect } from 'react';
 import {Route} from 'react-router-dom';
 import axios from 'axios';
 import MovieList from './Movies/MovieList.js';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
+import requester from 'easier-requests';
+
+function getMovies(setMovies){
+  async function _getMovies(setMovies) {
+    const uid = requester.createUniqueID();
+    await requester.get('http://localhost:5000/api/movies', uid);
+    setMovies(requester.response(uid).data);
+  }
+  _getMovies(setMovies);
+}
 
 function App () {
   requester.setOptions({throwOnFailure: true});
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovies(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    };
-    getMovies();
-  }, []);
+  useEffect(() => getMovies(setMovies), []);
 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
@@ -32,6 +31,9 @@ function App () {
       <SavedList list={[ /* This is stretch */]} />
       <Route exact path="/">
         <MovieList movies={movies}/>
+      </Route>
+      <Route path="/movies/:id">
+        <Movie />
       </Route>
     </div>
   );
